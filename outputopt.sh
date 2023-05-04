907a21328d7cc2a5607868484891498547ca2cb2
Chenyu Tang <tangcy98@163.com>
2023-05-04 14:28:29 +0800

    output optimization scripts comments

--- outputopt.sh
+++ outputopt.sh
@@ -1,3 +1,6 @@
+# This script sort and exchange the intermediate results(ni in verilog) 
+#  to make the output place as close as the input in memory.
+#  e.g. y0=n612;y1=n153;y2=n449; ==> y0=n129;y1=n130;y2=n131;
 benchmarks=(adder cavlc dec div int2float log2 max multiplier priority router sin sqrt)
 g++ outputopt.cpp -o outputopt
 for b in ${benchmarks[@]}
